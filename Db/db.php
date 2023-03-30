2d617bda3b390967cfffb58463ea1558b5255541
rennavittorio <renna.vittorio@gmail.com>
2023-03-30 15:54:16 +0200

    MLS BONUS 2 completed

---
+++ Db/db.php
@@ -0,0 +1,37 @@
+<?php
+
+$movies = [
+
+    $movie1 = [
+        'title' => 'the matrix',
+        'description' => 'neo scopre di vivere in un sogno, il mondo vero fa schifo',
+        'genres' => ['sci-fi', 'fantasy', 'action']
+    ],
+    $movie2 = [
+        'title' => 'the matrix 2',
+        'description' => 'il mondo vero fa ancora schifo, ma almeno neo ha la ragazza',
+        'genres' => ['sci-fi', 'fantasy', 'action']
+    ],
+    $movie3 = [
+        'title' => 'the matrix 3',
+        'description' => 'vincono gli umani sulle macchine grazie a neo',
+        'genres' => ['sci-fi', 'fantasy', 'action']
+    ],
+    $movie4 = [
+        'title' => 'la bella e la bestia',
+        'description' => 'un amore improbabile e l\'argenteria che rompe le scatole',
+        'genres' => ['cartoon']
+    ],
+    $movie5 = [
+        'title' => 'shining',
+        'description' => 'succedono cose brutte',
+        'genres' => ['horror', 'drama']
+    ],
+    $movie6 = [
+        'title' => 'basic instinct',
+        'description' => 'ne ricordo giusto una scena',
+        'genres' => ['action', 'drama']
+    ],
+
+
+];
--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php
 
 require_once './Models/Movie.php';
+require_once './Db/db.php';
 
-$input_title = ['the matrix', 'LOTR'];
-$input_description = ['uomini vs macchine: chi vincerà la battaglia?', 'hobbit, maghi e altre cose'];
-$input_genres = [['sci-fi', 'action', 'fantasy'], ['fantasy', 'avventure']];
+$films = [];
+foreach ($movies as $index => $movie) {
+    $film = new Movie($movie['title'], $movie['description'], $movie['genres']);
+    $films[] = $film;
+}
 
-$film1 = new Movie($input_title[0], $input_description[0], $input_genres[0]);
-$film2 = new Movie($input_title[1], $input_description[1], $input_genres[1]);
-// var_dump($film1);
+// var_dump($films);
+
+
+
+// $input_title = [$movies[0]['title'], 'LOTR'];
+// $input_description = [$movies[0]['description'], 'hobbit, maghi e altre cose'];
+// $input_genres = [$movies[0]['genres'], ['fantasy', 'avventure']];
+
+// $film1 = new Movie($input_title[0], $input_description[0], $input_genres[0]);
+// $film2 = new Movie($input_title[1], $input_description[1], $input_genres[1]);
+// // var_dump($film1);
 
 
 ?>
@@ -23,6 +34,7 @@ $film2 = new Movie($input_title[1], $input_description[1], $input_genres[1]);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/app.css">
     <title>Fake film generator</title>
 </head>
 
@@ -30,27 +42,29 @@ $film2 = new Movie($input_title[1], $input_description[1], $input_genres[1]);
 
     <div class="container">
 
-        <div class="row">
-            <div class="col-6">
+        <h1>Film</h1>
+
+        <div class="grid">
+            <?php
+            foreach ($films as $film) {
+            ?>
+
                 <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $film1->title; ?></h5>
-                        <p class="card-text"><?php echo $film1->description; ?></p>
-                        <p class="card-text"><?php echo $film1->getGenresText($film1->genres) ?></p>
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title"><?php echo $film->title; ?></h5>
+                        <p class="card-text"><?php echo $film->description; ?></p>
+                        <p class="card-text mt-auto"><?php echo $film->getGenresText($film->genres); ?></p>
                     </div>
                 </div>
 
-            </div>
-            <div class="col-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title"><?php echo $film2->title; ?></h5>
-                        <p class="card-text"><?php echo $film2->description; ?></p>
-                        <p class="card-text"><?php echo $film2->getGenresText($film1->genres) ?></p>
-                    </div>
-                </div>
 
-            </div>
+            <?php
+
+
+            }
+
+
+            ?>
 
 
         </div>
